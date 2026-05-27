03f80367dfdc414ac35068e5ac5929852b654ab0
JorgeDiezGracia <diezgraciajorge@gmail.com>
2026-05-27 13:10:49 +0200

    Initial project setup - libGDX Astra

---
+++ core/src/main/java/com/svalero/astra/Astra.java
@@ -0,0 +1,7 @@
+package com.svalero.astra;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Astra extends ApplicationAdapter {
+}
