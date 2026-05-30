0fb9d0b2e753d0056b4455c7e22799606e92b311
JorgeDiezGracia <diezgraciajorge@gmail.com>
2026-05-30 10:59:00 +0200

    Add MainMenuScreen - References #4

---
+++ core/src/main/java/com/svalero/astra/screens/MainMenuScreen.java
@@ -0,0 +1,120 @@
+package com.svalero.astra.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.svalero.astra.managers.ResourceManager;
+import com.svalero.astra.util.Constants;
+
+public class MainMenuScreen implements Screen {
+
+    private Game game;
+    private Stage stage;
+    private Skin skin;
+    private SpriteBatch batch;
+    private ResourceManager resourceManager;
+
+    public MainMenuScreen(Game game) {
+        this.game            = game;
+        this.resourceManager = ResourceManager.getInstance();
+    }
+
+    @Override
+    public void show() {
+        batch = new SpriteBatch();
+        stage = new Stage(new FitViewport(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT));
+        skin  = new Skin(Gdx.files.internal("uiskin.json"));
+        Gdx.input.setInputProcessor(stage);
+
+        Table table = new Table();
+        table.setFillParent(true);
+        table.center();
+        stage.addActor(table);
+
+        // Título
+        Label.LabelStyle titleStyle = new Label.LabelStyle();
+        titleStyle.font = resourceManager.fontLarge;
+        titleStyle.fontColor = Color.CYAN;
+        Label title = new Label("ASTRA", titleStyle);
+        table.add(title).padBottom(60).row();
+
+        // Botones
+        TextButton btnPlay = new TextButton("PLAY", skin);
+        TextButton btnConfig = new TextButton("OPTIONS", skin);
+        TextButton btnInstructions = new TextButton("HOW TO PLAY", skin);
+        TextButton btnExit = new TextButton("EXIT", skin);
+
+        table.add(btnPlay).width(250).height(55).padBottom(15).row();
+        table.add(btnConfig).width(250).height(55).padBottom(15).row();
+        table.add(btnInstructions).width(250).height(55).padBottom(15).row();
+        table.add(btnExit).width(250).height(55).row();
+
+        // Listeners
+        btnPlay.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                game.setScreen(new GameScreen(game));
+                dispose();
+            }
+        });
+
+        btnConfig.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                game.setScreen(new ConfigScreen(game));
+                dispose();
+            }
+        });
+
+        btnInstructions.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                game.setScreen(new InstructionsScreen(game));
+                dispose();
+            }
+        });
+
+        btnExit.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Gdx.app.exit();
+            }
+        });
+    }
+
+    @Override
+    public void render(float dt) {
+        Gdx.gl.glClearColor(0, 0, 0.1f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(dt);
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override public void pause() {}
+    @Override public void resume() {}
+    @Override public void hide() {}
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+        skin.dispose();
+        batch.dispose();
+    }
+}
